3b819a94f5a05ab27d3873c4c0c7708e9c11f74b
theprcode <jarvisparker3000@gmail.com>
2024-12-12 12:05:21 +0530

    Update Pr7Map.cpp

--- Pr7Map.cpp
+++ Pr7Map.cpp
@@ -1,47 +1,44 @@
-#include<iostream>
-#include<map>
+// Write a program in C++ to use map associative container. The keys will be the names of
+// states and the values will be the populations of the states. When the program runs, the
+// user is prompted to type the name of a state. The program then looks in the map,
+// using the state name as an index and returns the population of the state.
+
+#include <iostream>
+#include <map>
+#include <string>
+
 using namespace std;
 
-int main(){
-	string name;
-	long int po;
-	char ans;
-	
-	map<string ,long int>population;
-	population["Maharashtra"]=128256452;
-	population["Arunachal Pradesh"]=1327174;
-	population["Assam"]=32753261;
-	population["Bihar"]=95409077;
-	population["Uttar Pradesh"]=199812341;
-	population["West Bengal"]=91276115;
-	population["Madhya Pradesh"]=72626809;
-	population["Tamil Nadu"]=72147030;
-	population["Rajasthan"]=68548437;
-	
-	int n=population.size();
-	cout<<"\n\t Number of records:"<<n;
-	cout<<"\n";
-	
-	map<string ,long int>::iterator p;
-	cout<<"\n\t List of States with Population \n\t";
-	
-	for(p=population.begin();p!=population.end();p++){
-		cout<<p->first<<": "<<p->second<<"\n\t";
-	}
-	do{
-		cout<<"\n\t Enter name of State to searched";
-		cin>>name;
-		po=population[name];
-		if(po==0){
-			cout<<"Record not found";
-		}else{
-			cout<<"\n\tPopulation of "<<name<<" is "<<po;
-			cout<<"\n";
-		}
-		cout<<"\n\t Do you want to continue(y/n)";
-		cin>>ans;
-	}while(ans=='y');
-	
-	return 0;
+int main() {
+    // Create a map to store state names as keys and their populations as values
+    map<string, int> statePopulations;
+
+    // Insert some state-population pairs
+    statePopulations["California"] = 39538223;
+    statePopulations["Texas"] = 29145505;
+    statePopulations["Florida"] = 21538187;
+    statePopulations["New York"] = 20201249;
+    statePopulations["Illinois"] = 12812508;
+
+    string stateName;
+
+    // Prompt the user for a state name
+    cout << "Enter the name of a state: ";
+    getline(cin, stateName);
+
+    // Look up the state in the map
+    auto it = statePopulations.find(stateName);
+
+    if (it != statePopulations.end()) {
+        // State found, print the population
+        cout << "The population of " << stateName << " is " << it->second << ".\n";
+    } else {
+        // State not found
+        cout << "State not found in the database.\n";
+    }
+
+    return 0;
 }
+
+
 
