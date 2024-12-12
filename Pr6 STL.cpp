c0de66e080ae320f2c980294f092e51858c47068
theprcode <jarvisparker3000@gmail.com>
2024-12-12 12:05:58 +0530

    Update Pr6 STL.cpp

--- Pr6 STL.cpp
+++ Pr6 STL.cpp
@@ -1,63 +1,56 @@
-// Aim: Write C++ program using STL for sorting and searching user defined records such as personal records (Name, DOB, Telephone number etc) using vector container. 
+// Aim: Write C++ program using STL for sorting and searching user 
+//defined records such as personal records (Name, DOB, Telephone number etc) using vector container. 
+
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 using namespace std;
 
-class Person {
-public:
-    string name;
-    string dob;
-    string telephone;
-
-    Person(string n, string d, string t) : name(n), dob(d), telephone(t) {}
+struct record{
+	string Name,DOB,Telephone;
+	//< operator for sorting
+	bool operator<(const record &other) const {
+		return Name < other.Name ;
+		}
 };
 
-bool compareByName(const Person &a, const Person &b) {
-    return a.name < b.name;
-}
+int main(){
 
-void displayRecords(const vector<Person>& records) {
-    for (const auto& record : records) {
-        cout << "Name: " << record.name << ", DOB: " << record.dob << ", Telephone: " << record.telephone << endl;
-    }
-}
+	//define vector container
+	vector<record>records;
+	int n;
+	cout<<"Enter the number of records :: ";
+	cin>>n;
 
-int main() {
-    vector<Person> records;
-    int n;
-    cout << "Enter number of records: ";
-    cin >> n;
+	//Take input from user
+	for(int i=0;i<n;i++){
+		record temp;
+		cout<<"Enter name, DOB,telephone number of person :: ";
+		cin>>ws;
+		getline(cin,temp.Name);
+		getline(cin,temp.DOB);
+		getline(cin,temp.Telephone);
+		records.push_back(temp);
+	}
 
-    for (int i = 0; i < n; ++i) {
-        string name, dob, telephone;
-        cout << "Enter name: ";
-        cin >> name;
-        cout << "Enter DOB (DD-MM-YYYY): ";
-        cin >> dob;
-        cout << "Enter Telephone: ";
-        cin >> telephone;
-        records.push_back(Person(name, dob, telephone));
-    }
+	//sort records
+	sort(records.begin(),records.end());
+	for (const auto &r:records)
+		cout<<r.Name<<" "<<r.DOB<<" "<<r.Telephone<<endl;
 
-    sort(records.begin(), records.end(), compareByName);
-    
-    cout << "\nSorted records by name:\n";
-    displayRecords(records);
-
-    string searchName;
-    cout << "\nEnter name to search: ";
-    cin >> searchName;
-    auto it = find_if(records.begin(), records.end(), [&searchName](const Person& p) {
-        return p.name == searchName;
-    });
-
-    if (it != records.end()) {
-        cout << "Record found: Name: " << it->name << ", DOB: " << it->dob << ", Telephone: " << it->telephone << endl;
-    } else {
-        cout << "Record not found!" << endl;
-    }
-
-    return 0;
+	//Search Records
+	string SearchName;
+	cout<<"Enter the name to search :: ";
+	cin>>ws;
+	getline(cin,SearchName);
+	
+	auto it=lower_bound(records.begin(),records.end(),record{SearchName," "," "});
+	if (it != records.end() && it->Name == SearchName)
+        cout << "Found: " << it->Name << " " << it->DOB << " " << it->Telephone << endl;
+    else
+        cout << "Not found." << endl;
+	
+	return 0;
 }
